cb96de6a39159b691104f289ff4c9f350264bc16
MohammaOwais <142815375+MohammaOwais@users.noreply.github.com>
2023-08-21 20:12:01 +0530

    Add files via upload

---
+++ reverse22.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+	int a;
+	printf("Enter a 4-digit numeber...\n");
+	scanf("%d",&a);
+	int b=a%10;
+	int f1=a/1000;//1
+	int swap=b*1000;//4000
+	swap=swap+(a%1000);
+	
+	swap=swap-b;
+	swap=swap+f1;
+	
+	printf("Answer is=%d",swap);
+	
+	
+}
